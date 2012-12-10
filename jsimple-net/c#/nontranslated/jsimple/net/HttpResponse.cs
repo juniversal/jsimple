df2da59732ce2eea2b277f331ae59433f759874f
Bret Johnson <bret.johnson@nokia.com>
2012-12-10 12:24:38 -0500

    Added Resharper *.DotSettings files & a few missing nontranslated C# files

---
+++ jsimple-net/c#/nontranslated/jsimple/net/HttpResponse.cs
@@ -0,0 +1,27 @@
+﻿using jsimple.io;
+
+namespace jsimple.net
+{
+    public class HttpResponse : HttpResponseBase
+    {
+        public override int StatusCode
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public override string StatusMessage
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public override InputStream BodyStream
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public override string getHeader(string headerName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
