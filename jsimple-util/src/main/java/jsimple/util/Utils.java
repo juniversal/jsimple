9e860dce931b6e625d6d69a5108b670d174b654e
bkwon <bong.kwon@nokia.com>
2013-05-03 15:50:29 -0400

    add RFC3339 parsing.

--- jsimple-util/src/main/java/jsimple/util/Utils.java
+++ jsimple-util/src/main/java/jsimple/util/Utils.java
@@ -1,5 +1,7 @@
 package jsimple.util;
 
+import java.text.SimpleDateFormat;
+import java.util.*;
 /**
  * Created with IntelliJ IDEA.
  *
@@ -29,4 +31,88 @@ public class Utils {
 
         return byteArray;
     }
-}
+
+    /*
+    Copyright 2007, Chad Okere (ceothrow1 at gmail dotcom)
+    OMG NO WARRENTY EXPRESSED OR IMPLIED!!!1
+    */
+     public static java.util.Date parseRFC3339Date(String datestring) throws java.text.ParseException, IndexOutOfBoundsException{
+            Date d = new Date();
+
+            //if there is no time zone, we don't need to do any special parsing.
+            if(datestring.endsWith("Z")){
+                try{
+                    SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");//spec for RFC3339
+                    d = s.parse(datestring);
+                }
+                catch(java.text.ParseException pe){//try again with optional decimals
+                    SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'");//spec for RFC3339 (with fractional seconds)
+                    s.setLenient(true);
+                    d = s.parse(datestring);
+                }
+                return d;
+            }
+
+            //step one, split off the timezone.
+            String firstpart = datestring.substring(0,datestring.lastIndexOf('-'));
+            String secondpart = datestring.substring(datestring.lastIndexOf('-'));
+
+            //step two, remove the colon from the timezone offset
+            secondpart = secondpart.substring(0,secondpart.indexOf(':')) + secondpart.substring(secondpart.indexOf(':')+1);
+            datestring  = firstpart + secondpart;
+            SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");//spec for RFC3339
+            try{
+                d = s.parse(datestring);
+            }
+            catch(java.text.ParseException pe){//try again with optional decimals
+                s = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ");//spec for RFC3339 (with fractional seconds)
+                s.setLenient(true);
+                d = s.parse(datestring);
+            }
+            return d;
+     }
+
+    public static long parseRFC3339Long(String datestring) throws java.text.ParseException, IndexOutOfBoundsException{
+        Date d = new Date();
+
+        //if there is no time zone, we don't need to do any special parsing.
+        if(datestring.endsWith("Z")){
+            try{
+                SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");//spec for RFC3339
+                d = s.parse(datestring);
+            }
+            catch(java.text.ParseException pe){//try again with optional decimals
+                SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'");//spec for RFC3339 (with fractional seconds)
+                s.setLenient(true);
+                d = s.parse(datestring);
+            }
+            return d.getTime();
+        }
+
+        //step one, split off the timezone.
+        String firstpart = datestring.substring(0,datestring.lastIndexOf('-'));
+        String secondpart = datestring.substring(datestring.lastIndexOf('-'));
+
+        //step two, remove the colon from the timezone offset
+        secondpart = secondpart.substring(0,secondpart.indexOf(':')) + secondpart.substring(secondpart.indexOf(':')+1);
+        datestring  = firstpart + secondpart;
+        SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");//spec for RFC3339
+        try{
+            d = s.parse(datestring);
+        }
+        catch(java.text.ParseException pe){//try again with optional decimals
+            s = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ");//spec for RFC3339 (with fractional seconds)
+            s.setLenient(true);
+            d = s.parse(datestring);
+        }
+        return d.getTime();
+    }
+
+    public static String makeRFC3339String(long timeValue){
+        Date d = new Date(timeValue);
+
+        String rfc3339TimeStamp = new SimpleDateFormat("yyyy-MM-dd'T'h:m:ssZ").format(new Date());
+
+        return rfc3339TimeStamp;
+    }
+ }
