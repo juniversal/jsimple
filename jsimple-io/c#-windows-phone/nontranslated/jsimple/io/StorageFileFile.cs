b772dbf313f192100fb7d6f72f67d24ee33adec2
Bret Johnson <bret.johnson@nokia.com>
2013-05-13 17:22:38 -0400

    Fixed build error (typo, apparently)

--- jsimple-io/c#-windows-phone/nontranslated/jsimple/io/StorageFileFile.cs
+++ jsimple-io/c#-windows-phone/nontranslated/jsimple/io/StorageFileFile.cs
@@ -46,7 +46,7 @@ namespace jsimple.io
 
             // Use the default buffer for AsStreamForWrite()
             return new DotNetStreamOutputStream(outputStream.AsStreamForWrite());
-        }}
+        }
 
         public override void delete()
         {
