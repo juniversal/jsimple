530330a63c415006e9d94abee536f1eb73b39d1b
Bret Johnson <bret.johnson@nokia.com>
2013-05-28 16:03:24 -0400

    Added a couple missing files

---
+++ jsimple-logging-api/c#/jsimple/logging/LogEnterLeave.cs
@@ -0,0 +1,12 @@
+namespace jsimple.logging
+{
+
+	/// <summary>
+	/// @author Bret Johnson
+	/// @since 5/27/13 12:27 AM
+	/// </summary>
+	public abstract class LogEnterLeave : jsimple.lang.AutoCloseable
+	{
+	}
+
+}
