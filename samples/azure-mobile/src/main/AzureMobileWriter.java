332165fb9fbfe630e2dbfa1d8ccbd118477e3542
moldo- <java.lang.system.err@gmail.com>
2014-12-08 16:10:14 +0200

    JsonParsing and sample refactoring

--- samples/azure-mobile/src/main/AzureMobileWriter.java
+++ samples/azure-mobile/src/main/AzureMobileWriter.java
@@ -1,5 +1,6 @@
 import jsimple.net.HttpRequest;
 import jsimple.net.HttpResponse;
+import jsimple.util.HashMap;
 
 /**
  * @author Dorin Suletea
@@ -7,30 +8,60 @@ import jsimple.net.HttpResponse;
  */
 public class AzureMobileWriter {
 
-	/*uses the app id to instert data into Table1*/
-	public static void insertDataWithAppID() throws Exception {
-		HttpRequest req = HttpRequest.create(Constants.DB_PATH);
-		
+	/**
+	 * Inserts data into the specified table using appID (not the azure token)
+	 * dataToInsert is a key-value mapping that represents what data to insert
+	 * into what columns hashmap.key = column name hashmap.value = value to
+	 * insert
+	 * 
+	 * Table1 structure is > ID: int (autoincrement)(primary key) A: varchar B:
+	 * varchar Hence the dataToInsert will contain 2 pairs of data :
+	 * (A,myAInsertData) and (B,myBinsertData)
+	 * 
+	 * @param table
+	 *            the name of the table you want to insert into
+	 * @param dataToInsert
+	 *            field->value mapping
+	 */
+	public static boolean insertDataWithAppID(String table, HashMap<String, String> dataToInsert) {
+		HttpRequest req = HttpRequest.create(Constants.DB_PATH + table);
+
 		req.setMethod(HttpRequest.METHOD_POST);
 		req.setContentTypeAndAcceptHeaders("application/json", "application/json");
-		req.setHeader("X-ZUMO-APPLICATION", Constants.AZURE_APP_ID);// <--- the azure-mobile app id
-		
-		//request body contains the json for the new data to be inserted
-		//currently hardcoded json is "{"A":"5555","B":"22222"}"
-		// Table1 structure is 
-		//
-		// ID: int (autoincrement)(primary key)
-		// A: varchar
-		// B: varchar
-		jsimple.io.Utf8OutputStreamWriter out = new jsimple.io.Utf8OutputStreamWriter(req.createRequestBodyStream());
-		String reqStr = "{\"A\":\"5555\",\"B\":\"22222\"}";
+		req.setHeader("X-ZUMO-APPLICATION", Constants.AZURE_APP_ID);// <--- the
+																	// azure-mobile
+																	// app id
 
-		out.write(reqStr);
+		jsimple.io.Utf8OutputStreamWriter out = new jsimple.io.Utf8OutputStreamWriter(req.createRequestBodyStream());
+
+		StringBuffer reqStr = new StringBuffer();
+
+		// example reqStr = "{\"A\":\"5555\",\"B\":\"22222\"}";
+		reqStr.append("{");
+		for (String key : dataToInsert.keySet()) {
+			reqStr.append("\"" + key + "\"");
+			reqStr.append(":");
+			reqStr.append("\"" + dataToInsert.get(key) + "\"");
+			reqStr.append(",");
+		}
+		// remove the last comma
+		reqStr.deleteCharAt(reqStr.length() - 1);
+
+		reqStr.append("}");
+
+		System.out.println("Insert request " + reqStr.toString());
+
+		out.write(reqStr.toString());
 		out.flush();
 		out.close();
-		
+
 		HttpResponse resp = req.send();
-		
-		System.out.println(resp.getStatusCode());
+
+		System.out.println("Insert resp code " + resp.getStatusCode());
+		boolean sucess = false;
+		if (resp.getStatusCode() == 201) {
+			sucess = true;
+		}
+		return sucess;
 	}
 }
