3da1e36276f01b667589cef2808bf454bf3e2d6a
moldo- <java.lang.system.err@gmail.com>
2014-12-12 16:28:40 +0200

    added comments and increased post range

--- samples/special-friends/src/com/special_friend/facebook/FacebookConnector.java
+++ samples/special-friends/src/com/special_friend/facebook/FacebookConnector.java
@@ -2,6 +2,11 @@ package com.special_friend.facebook;
 
 import jsimple.net.HttpRequest;
 
+/**
+ * Facebook raw-data extractor 
+ * Specialized class that composes parameters and request specific strings in order to form 
+ * a valid facebook request, after the request is sent data is extracted and returned as raw jsons
+ */
 public class FacebookConnector extends Connector {
 	private String authToken;
 
@@ -9,14 +14,24 @@ public class FacebookConnector extends Connector {
 		this.authToken = authToken;
 	}
 
-	public String getMyTimeline() {
-		String request = "https://graph.facebook.com/me/home?access_token=" + authToken;
+	/**
+	 * Extracts the posts from the user's timeline posts 
+	 * @param postLimit how many posts to extract from the timeline , as the number increases the slower 
+	 * the posts will be returned
+	 * @return raw posts as json
+	 */
+	public String getMyTimeline(Integer postLimit) {
+		String request = "https://graph.facebook.com/me/home?limit="+postLimit+"&access_token=" + authToken;
 		HttpRequest req = HttpRequest.create(request);
 
 		String response = sendRequest(req);
 		return response;
 	}
 
+	/**
+	 * Extracts the user's taggable friends friends
+	 * @return raw friends as json
+	 */
 	public String getFriendList() {
 		String request = "https://graph.facebook.com/me/taggable_friends?access_token=" + authToken;
 		HttpRequest req = HttpRequest.create(request);
@@ -24,7 +39,10 @@ public class FacebookConnector extends Connector {
 		String response = sendRequest(req);
 		return response;
 	}
-	
+	/**
+	 * Extracts the id of the person using the application 
+	 * @return user's info json
+	 */
 	public String getMyInfo(){
 		String request = "https://graph.facebook.com/me?access_token=" + authToken;
 		HttpRequest req = HttpRequest.create(request);
