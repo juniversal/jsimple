5e24af961ca955172c4ce5a731f2d25eb9ba3274
Bret Johnson <bret.johnson@microsoft.com>
2015-01-12 03:06:56 -0500

    Fixed up HashSetTest

--- jsimple-util/src/test/java/jsimple/util/HashSetTest.java
+++ jsimple-util/src/test/java/jsimple/util/HashSetTest.java
@@ -27,262 +27,213 @@
 package jsimple.util;
 
 import jsimple.unit.UnitTest;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Iterator;
-
 public class HashSetTest extends UnitTest {
-	private HashSet<Object> hs;
-	static Object[] objArray;
+    private HashSet<Object> hs;
+    static Object[] objArray;
 
-	static {
-		objArray = new Object[1000];
-		for (int i = 0; i < objArray.length; i++)
-			objArray[i] = new Integer(i);
-	}
+    @Override public void setUp() {
+        objArray = new Object[1000];
+        for (int i = 0; i < objArray.length; i++)
+            objArray[i] = i;
 
-	@Test
-	public void testConstructor() {
-		HashSet<Object> hs2 = new HashSet<Object>();
-		assertEquals("Created incorrect HashSet", 0, hs2.size());
-	}
+        hs = new HashSet<Object>();
+        for (int i = 0; i < objArray.length; i++)
+            hs.add(objArray[i]);
+    }
 
-	@Test
-	public void testConstructorSize() {
-		// Test for method java.util.HashSet(int)
-		HashSet<Object> hs2 = new HashSet<Object>(5);
-		assertEquals("Created incorrect HashSet", 0, hs2.size());
-		try {
-			new HashSet<Object>(-1);
-			fail("Failed to throw IllegalArgumentException for capacity < 0");
-		} catch (IllegalArgumentException e) {
-			// expected
-		}
-	}
+    @Test public void testConstructor() {
+        HashSet<Object> hs2 = new HashSet<Object>();
+        assertEquals("Created incorrect HashSet", 0, hs2.size());
+    }
 
-	@Test
-	public void testConstructorLoadFactor() {
-		// Test for method java.util.HashSet(int, float)
-		HashSet<Object> hs2 = new HashSet<Object>(5, (float) 0.5);
-		assertEquals("Created incorrect HashSet", 0, hs2.size());
-		try {
-			new HashSet<Object>(0, 0);
-			fail("Failed to throw IllegalArgumentException for initial load factor <= 0");
-		} catch (IllegalArgumentException e) {
-			// expected
-		}
+    @Test public void testConstructorSize() {
+        // Test for method java.util.HashSet(int)
+        HashSet<Object> hs2 = new HashSet<Object>(5);
+        assertEquals("Created incorrect HashSet", 0, hs2.size());
+        try {
+            new HashSet<Object>(-1);
+            fail("Failed to throw IllegalArgumentException for capacity < 0");
+        } catch (ProgrammerError e) {
+            // expected
+        }
+    }
 
-	}
+    @Test public void testConstructorLoadFactor() {
+        // Test for method java.util.HashSet(int, float)
+        HashSet<Object> hs2 = new HashSet<Object>(5, (float) 0.5);
+        assertEquals("Created incorrect HashSet", 0, hs2.size());
+        try {
+            new HashSet<Object>(0, 0);
+            fail("Failed to throw IllegalArgumentException for initial load factor <= 0");
+        } catch (ProgrammerError e) {
+            // expected
+        }
 
-	@Test
-	public void testConstructorFromCollection() {
-		// Test for method java.util.HashSet(java.util.Collection)
-		HashSet<Object> hs2 = HashSet.create(objArray);
+    }
 
-		for (Object anObjArray : objArray) {
-			assertTrue("HashSet does not contain correct elements", hs.contains(anObjArray));
-		}
-		assertTrue("HashSet created from collection incorrect size", hs2.size() == objArray.length);
-	}
+    @Test public void testConstructorFromCollection() {
+        // Test for method java.util.HashSet(java.util.Collection)
+        HashSet<Object> hs2 = new HashSet<Object>(objArray);
 
-	@Test
-	public void testAdd() {
-		// Test for method boolean java.util.HashSet.add(java.lang.Object)
-		int size = hs.size();
-		hs.add(new Integer(8));
+        for (Object anObjArray : objArray) {
+            assertTrue("HashSet does not contain correct elements", hs.contains(anObjArray));
+        }
+        assertTrue("HashSet created from collection incorrect size", hs2.size() == objArray.length);
+    }
 
-		assertTrue("Added element already contained by set", hs.size() == size);
-		hs.add(new Integer(-9));
+    @Test public void testAdd() {
+        // Test for method boolean java.util.HashSet.add(java.lang.Object)
+        int size = hs.size();
+		hs.add(8);
 
-		assertTrue("Failed to increment set size after add", hs.size() == size + 1);
-		assertTrue("Failed to add element to set", hs.contains(new Integer(-9)));
-	}
+        assertTrue("Added element already contained by set", hs.size() == size);
+		hs.add(-9);
 
-	@SuppressWarnings("unchecked")
-	@Test
-	public void testClear() {
-		// Test for method void java.util.HashSet.clear()
-		Set<Object> orgSet = new HashSet<Object>(hs);
-		hs.clear();
-		Iterator<Object> i = orgSet.iterator();
-		assertEquals("Returned non-zero size after clear", 0, hs.size());
+        assertTrue("Failed to increment set size after add", hs.size() == size + 1);
+		assertTrue("Failed to add element to set", hs.contains(-9));
+    }
 
-		while (i.hasNext()) {
-			assertTrue("Failed to clear set", !hs.contains(i.next()));
-		}
-	}
+    @Test
+    public void testClear() {
+        // Test for method void java.util.HashSet.clear()
+        Set<Object> orgSet = new HashSet<Object>(hs);
+        hs.clear();
+        Iterator<Object> i = orgSet.iterator();
+        assertEquals("Returned non-zero size after clear", 0, hs.size());
 
-	@Test
-	public void testContainsNull() {
-		// Test for method boolean java.util.HashSet.contains(java.lang.Object)
-		assertTrue("Returned false for valid object", hs.contains(objArray[90]));
-		assertTrue("Returned true for invalid Object", !hs.contains(new Object()));
+        while (i.hasNext()) {
+            assertTrue("Failed to clear set", !hs.contains(i.next()));
+        }
+    }
 
-		HashSet<Object> s = new HashSet<Object>();
-		s.add(null);
-		assertTrue("Cannot handle null", s.contains(null));
-	}
+    @Test public void testContainsNull() {
+        // Test for method boolean java.util.HashSet.contains(java.lang.Object)
+        assertTrue("Returned false for valid object", hs.contains(objArray[90]));
+        assertTrue("Returned true for invalid Object", !hs.contains(new Object()));
+    }
 
-	@Test
-	public void testIsEmpty() {
-		// Test for method boolean java.util.HashSet.isEmpty()
-		assertTrue("Empty set returned false", new HashSet<Object>().isEmpty());
-		assertTrue("Non-empty set returned true", !hs.isEmpty());
-	}
+    @Test public void testIsEmpty() {
+        // Test for method boolean java.util.HashSet.isEmpty()
+        assertTrue("Empty set returned false", new HashSet<Object>().isEmpty());
+        assertTrue("Non-empty set returned true", !hs.isEmpty());
+    }
 
-	@Test
-	public void testIterator() {
-		// Test for method java.util.Iterator java.util.HashSet.iterator()
-		Iterator<Object> i = hs.iterator();
-		int x = 0;
+    @Test public void testIterator() {
+        // Test for method java.util.Iterator java.util.HashSet.iterator()
+        Iterator<Object> i = hs.iterator();
+        int x = 0;
 
-		while (i.hasNext()) {
-			assertTrue("Failed to iterate over all elements", hs.contains(i.next()));
-			++x;
-		}
-		assertTrue("Returned iteration of incorrect size", hs.size() == x);
+        while (i.hasNext()) {
+            assertTrue("Failed to iterate over all elements", hs.contains(i.next()));
+            ++x;
+        }
+        assertTrue("Returned iteration of incorrect size", hs.size() == x);
+    }
 
-		HashSet<Object> s = new HashSet<Object>();
-		s.add(null);
-		assertNull("Cannot handle null", s.iterator().next());
-	}
+    @Test public void testRemove() {
+        // Test for method boolean java.util.HashSet.remove(java.lang.Object)
+        int size = hs.size();
+        hs.remove(98);
+        assertTrue("Failed to remove element", !hs.contains(98));
+        assertTrue("Failed to decrement set size", hs.size() == size - 1);
+    }
 
-	@Test
-	public void testRemove() {
-		// Test for method boolean java.util.HashSet.remove(java.lang.Object)
-		int size = hs.size();
-		hs.remove(new Integer(98));
-		assertTrue("Failed to remove element", !hs.contains(new Integer(98)));
-		assertTrue("Failed to decrement set size", hs.size() == size - 1);
+    /**
+     * @tests java.util.HashSet#size()
+     */
+    @Test public void testSize() {
+        // Test for method int java.util.HashSet.size()
+        assertTrue("Returned incorrect size", hs.size() == objArray.length);
+        hs.clear();
+        assertEquals("Cleared set returned non-zero size", 0, hs.size());
+    }
 
-		HashSet<Object> s = new HashSet<Object>();
-		s.add(null);
-		assertTrue("Cannot handle null", s.remove(null));
-	}
+    /**
+     * @tests java.util.AbstractCollection#toString()
+     */
+/*
+    @Test public void testToString() {
+        HashSet<Object> s = new HashSet<Object>();
+        s.add(s);
+        String result = s.toString();
+        assertTrue("should contain self ref", result.contains("(this"));
+    }
+*/
 
-	/**
-	 * @tests java.util.HashSet#size()
-	 */
-	@Test
-	public void testSize() {
-		// Test for method int java.util.HashSet.size()
-		assertTrue("Returned incorrect size", hs.size() == (objArray.length + 1));
-		hs.clear();
-		assertEquals("Cleared set returned non-zero size", 0, hs.size());
-	}
+    @Test public void testContainDistictType() {
+        HashSet<Object> s = new HashSet<Object>();
+        String data = "teststr";
+        s.add(data);
 
-	/**
-	 * @tests java.util.AbstractCollection#toString()
-	 */
-	@Test
-	public void testToString() {
-		HashSet<Object> s = new HashSet<Object>();
-		s.add(s);
-		String result = s.toString();
-		assertTrue("should contain self ref", result.indexOf("(this") > -1);
-	}
+        Object q = data;
+        assertTrue("Cannot handle null", s.contains(q));
+    }
 
-	/**
-	 * Sets up the fixture, for example, open a network connection. This method
-	 * is called before a test is executed.
-	 */
-	@Before
-	public void setUp() {
-		hs = new HashSet<Object>();
-		for (int i = 0; i < objArray.length; i++)
-			hs.add(objArray[i]);
-		hs.add(null);
-	}
+    @Test public void testAddAll() {
+        HashSet<String> sut = new HashSet<String>();
+        ArrayList<String> insertData = new ArrayList<String>();
+        insertData.add("1");
+        insertData.add("2");
 
-	/**
-	 * Tears down the fixture, for example, close a network connection. This
-	 * method is called after a test is executed.
-	 */
-	@After
-	public void tearDown() {
-	}
+        sut.add("one");
+        sut.addAll(insertData);
 
-	@Test
-	public void testContainDistictType() {
-		HashSet<Object> s = new HashSet<Object>();
-		String data = "teststr";
-		s.add(data);
+        assertEquals(3, sut.size());
+        assertTrue(sut.contains("one"));
+        assertTrue(sut.contains("1"));
+        assertTrue(sut.contains("2"));
+    }
 
-		Object q = data;
-		assertTrue("Cannot handle null", s.contains(q));
-	}
+    @Test public void testContainsAll() {
+        HashSet<String> sut = new HashSet<String>();
+        sut.add("2");
+        sut.add("1");
 
-	@Test
-	public void testAddAll() {
-		HashSet<String> sut = new HashSet<String>();
-		ArrayList<String> insertData = new ArrayList<String>();
-		insertData.add("1");
-		insertData.add("2");
+        // reverser order
+        ArrayList<String> testData = new ArrayList<String>();
+        testData.add("1");
+        testData.add("2");
 
-		sut.add("one");
-		sut.addAll(insertData);
+        assertTrue(sut.containsAll(testData));
+    }
 
-		assertEquals(3, sut.size());
-		assertTrue(sut.contains("one"));
-		assertTrue(sut.contains("1"));
-		assertTrue(sut.contains("2"));
-	}
+    @Test public void testRemoveNegative() {
+        HashSet<String> sut = new HashSet<String>();
+        sut.add("2");
+        sut.add("1");
 
-	@Test
-	public void testContainsAll() {
-		HashSet<String> sut = new HashSet<String>();
-		sut.add("2");
-		sut.add("1");
+        sut.remove("5");
 
-		// reverser order
-		ArrayList<String> testData = new ArrayList<String>();
-		testData.add("1");
-		testData.add("2");
+        assertEquals(2, sut.size());
+    }
 
-		assertTrue(sut.containsAll(testData));
-	}
+    @Test public void testDuplication() {
+        HashSet<String> sut = new HashSet<String>();
+        sut.add("1");
+        sut.add("1");
 
-	@Test
-	public void testRemoveNegative() {
-		HashSet<String> sut = new HashSet<String>();
-		sut.add("2");
-		sut.add("1");
+        assertEquals(1, sut.size());
+    }
 
-		sut.remove("5");
+    @Test public void testRetainAll() {
+        HashSet<String> sut = new HashSet<String>();
+        sut.add("2");
+        sut.add("1");
+        sut.add("3");
 
-		assertEquals(2, sut.size());
-	}
+        // reverser order
+        ArrayList<String> testData = new ArrayList<String>();
+        testData.add("1");
+        testData.add("2");
 
-	@Test
-	public void testDuplication() {
-		HashSet<String> sut = new HashSet<String>();
-		sut.add("1");
-		sut.add("1");
+        sut.retainAll(testData);
 
-		assertEquals(1, sut.size());
-	}
+        assertEquals(2, sut.size());
+        assertTrue(sut.contains("1"));
+        assertTrue(sut.contains("2"));
+        assertFalse(sut.contains("3"));
 
-	@Test
-	public void testRetainAll() {
-		HashSet<String> sut = new HashSet<String>();
-		sut.add("2");
-		sut.add("1");
-		sut.add("3");
-
-		// reverser order
-		ArrayList<String> testData = new ArrayList<String>();
-		testData.add("1");
-		testData.add("2");
-
-		sut.retainAll(testData);
-
-		assertEquals(2, sut.size());
-		assertTrue(sut.contains("1"));
-		assertTrue(sut.contains("2"));
-		assertFalse(sut.contains("3"));
-
-	}
+    }
 }
