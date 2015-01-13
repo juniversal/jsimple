417b7b16902d70e8f8ae43e97f11099a54848e31
Bret Johnson <bret.johnson@microsoft.com>
2015-01-12 21:23:34 -0500

    Updated jsimple util copyright

--- jsimple-util/src/main/java/jsimple/util/Collection.java
+++ jsimple-util/src/main/java/jsimple/util/Collection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014 Microsoft Mobile.  All Rights Reserved.
+ * Copyright (c) 2012-2015 Microsoft Mobile.  All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
